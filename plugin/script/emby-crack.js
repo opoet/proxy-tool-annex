d32d22745edaf0091cb1155811234d62d03d8816
ZE XUAN FANG <i@opoet.cn>
2025-12-12 11:46:41 +0800

    update

--- plugin/script/emby-crack.js
+++ plugin/script/emby-crack.js
@@ -6,39 +6,37 @@
 
 const REWRITE_RULES = {
   "/admin/service/registration/validateDevice":
-    '{"cacheExpirationDays":999,"message":"Device Valid","resultCode":"GOOD"}',
+    '{"cacheExpirationDays":365,"message":"Device Valid","resultCode":"GOOD"}',
   "/admin/service/registration/validate":
     '{"featId":"","registered":true,"expDate":"2099-01-01","key":""}',
   "/admin/service/registration/getStatus":
-    '{"planType":"Lifetime","deviceStatus":0,"subscriptions":[]}',
+    '{"deviceStatus":"0","planType":"Lifetime","subscriptions":{}}',
   "/admin/service/appstore/register":
     '{"featId":"","registered":true,"expDate":"2099-01-01","key":""}',
   "/emby/Plugins/SecurityInfo":
-    '{"SupporterKey":"","isMBSupporter":true}',
+    '{"SupporterKey":"","IsMBSupporter":true}',
 };
 
-const URL_PATTERN = /https?:\/\/mb3admin\.com(\/[^?#]+)/;
+const URL_PATTERN = /https?:\/\/mb3admin\.com(\/[^?#]*)/;
 
 function crackEmbyVerifyRes(response, body) {
-  return Object.assign(response, {
-    status: 200,
-    headers: {
-      ...response.headers,
-      "Content-Type": "application/json",
-      "Charset": "utf-8",
-    },
-    body,
-  });
+  response.status = 200;
+  response.headers = {
+    ...response.headers,
+    "Content-Type": "application/json; charset=utf-8",
+  };
+  response.body = body;
+  return response;
 }
 
 function processResponse(response) {
   const match = $request.url.match(URL_PATTERN);
   if (match) {
-    const urlPath = match[1];
+    const urlPath = match[1].replace(/\/$/, "");
     const rewriteBody = REWRITE_RULES[urlPath];
     if (rewriteBody) {
       crackEmbyVerifyRes(response, rewriteBody);
-      console.log(`Rewrite Emby verify: ${urlPath}`);
+      console.log(`[Emby Crack] Rewrite: ${urlPath}`);
     }
   }
   return response;
