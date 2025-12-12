3abe3f030b72f6fbf82163dec494cd75ba239eaa
ZE XUAN FANG <i@opoet.cn>
2025-12-12 11:35:41 +0800

    update script

--- plugin/script/emby-crack.js
+++ plugin/script/emby-crack.js
@@ -1,39 +1,44 @@
 /*
   Author: opoet
-  Date: 2023-03-26
-  Description: Emby crack script
+  Date: 2025-12-12
+  Description: Emby verify rewrite
 */
 
-function crackEmbyVerifyRes(response, res) {
-  response.headers["Content-Type"] = "application/json";
-  response.headers["Charset"] = "utf-8";
-  response.status = 200;
-  response.body = res;
-  console.log("Rewrite Emby verify URL response successfully");
+const REWRITE_RULES = {
+  "/admin/service/registration/validateDevice":
+    '{"cacheExpirationDays":999,"message":"Device Valid","resultCode":"GOOD"}',
+  "/admin/service/registration/validate":
+    '{"featId":"","registered":true,"expDate":"2099-01-01","key":""}',
+  "/admin/service/registration/getStatus":
+    '{"planType":"Lifetime","deviceStatus":0,"subscriptions":[]}',
+  "/admin/service/appstore/register":
+    '{"featId":"","registered":true,"expDate":"2099-01-01","key":""}',
+  "/emby/Plugins/SecurityInfo":
+    '{"SupporterKey":"","isMBSupporter":true}',
+};
+
+const URL_PATTERN = /https?:\/\/mb3admin\.com(\/[^?#]+)/;
+
+function crackEmbyVerifyRes(response, body) {
+  return Object.assign(response, {
+    status: 200,
+    headers: {
+      ...response.headers,
+      "Content-Type": "application/json",
+      "Charset": "utf-8",
+    },
+    body,
+  });
 }
 
 function processResponse(response) {
-  const { url } = $request;
-  const regex = /https?:\/\/mb3admin\.com(\/[^?]+)\?/;
-  const match = url.match(regex);
-
+  const match = $request.url.match(URL_PATTERN);
   if (match) {
-    const rewriteRules = {
-      "/admin/service/registration/validateDevice":
-        '{"cacheExpirationDays":999,"message":"Device Valid","resultCode":"GOOD"}',
-      "/admin/service/registration/validate":
-        '{"featId": "","registered": true,"expDate": "2099-01-01","key": ""}',
-      "/admin/service/registration/getStatus":
-        '{planType: "Lifetime", deviceStatus: 0, subscriptions: []}',
-      "/admin/service/appstore/register":
-        '{"featId": "","registered": true,"expDate": "2099-01-01","key": ""}',
-      "/emby/Plugins/SecurityInfo": '{SupporterKey: "", IsMBSupporter: true}',
-    };
-
     const urlPath = match[1];
-
-    if (rewriteRules.hasOwnProperty(urlPath)) {
-      crackEmbyVerifyRes(response, rewriteRules[urlPath]);
+    const rewriteBody = REWRITE_RULES[urlPath];
+    if (rewriteBody) {
+      crackEmbyVerifyRes(response, rewriteBody);
+      console.log(`Rewrite Emby verify: ${urlPath}`);
     }
   }
   return response;
